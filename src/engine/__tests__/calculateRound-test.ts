33d91379afe6078bcdd3b34971d460d5501e103a
Tolga Oğuz <tolguz@icloud.com>
2026-07-23 20:44:53 +0300

    test(engine): add Jest test infrastructure

---
+++ src/engine/__tests__/calculateRound-test.ts
@@ -0,0 +1,7 @@
+import { calculateRound } from '../calculateRound';
+
+describe('calculateRound', () => {
+  it('can be imported', () => {
+    expect(typeof calculateRound).toBe('function');
+  });
+});
