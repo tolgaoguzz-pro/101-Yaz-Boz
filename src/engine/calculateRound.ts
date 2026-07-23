dcacb3d690344f4d393b959d18e555086d164208
Tolga Oğuz <tolguz@icloud.com>
2026-07-23 18:23:38 +0300

    feat(engine): implement normal finish scoring

--- src/engine/calculateRound.ts
+++ src/engine/calculateRound.ts
@@ -1,22 +1,53 @@
-import { RoundInput } from './models';
-import { ScoreRules } from './rules';
+import { PlayerRoundInput, RoundInput } from './models';
+import { OpenBaseRules, ScoreRules } from './rules';
+
+export type PlayerRoundScore = {
+  playerId: string;
+  score: number;
+};
 
 export type CalculateRoundResult = {
-  // TODO: player round scores
-  // TODO: team round scores / finish bonus application
+  players: PlayerRoundScore[];
+  /** Applied only when finishType is 'normal'; otherwise 0. */
+  finishTeamBonus: number;
 };
+
+function basePenalty(
+  player: PlayerRoundInput,
+  openBase: OpenBaseRules,
+): number {
+  switch (player.openType) {
+    case 'didNotOpen':
+      return openBase.didNotOpenPenalty;
+    case 'series':
+      return player.remainingTilePoints * openBase.seriesTileSumMultiplier;
+    case 'doubles':
+      return player.remainingTilePoints * openBase.doublesTileSumMultiplier;
+  }
+}
+
+function normalFinishTeamBonus(
+  finishType: RoundInput['finish']['finishType'],
+  rules: ScoreRules,
+): number {
+  if (finishType !== 'normal') {
+    return 0;
+  }
+
+  return rules.finishTeamBonus.normal;
+}
 
 export function calculateRound(
   input: RoundInput,
   rules: ScoreRules,
 ): CalculateRoundResult {
-  // TODO: compute each player's base penalty from openType
-  // TODO: add okey / wrong-open / playable-discard / manual extras
-  // TODO: apply opponent finish multiplier (respect extraPenaltyTiming)
-  // TODO: apply finisher partner penalty rule
-  // TODO: apply finish team bonus when finishType is not 'none'
-  void input;
-  void rules;
+  const players = input.players.map((player) => ({
+    playerId: player.playerId,
+    score: basePenalty(player, rules.openBase),
+  }));
 
-  return {};
+  return {
+    players,
+    finishTeamBonus: normalFinishTeamBonus(input.finish.finishType, rules),
+  };
 }
